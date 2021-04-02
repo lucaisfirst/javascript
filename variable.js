9faae7a53bacdae8416afb65ba09ed8e8c873d5b
lukaisfirst <knj9417@gmail.com>
2021-04-03 03:37:37 +0900

    first commit

---
+++ variable.js
@@ -0,0 +1,67 @@
+//1. Use strict 
+//added in ES5
+//javascript is flexible --> flexible === dangerous
+//in vanila js 'use strict' is moer helpful to parsing, 조금 더 상식적으로 사용 가능 
+'use strict';
+
+//2. Variable 
+//let (added in ES6)
+//글로벌한 변수들은 최소화
+//블록으로 가둘 수 있다. 
+let globalName = 'global name';
+{
+    let name = 'luca';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+    console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+//var (don't ever use this!) 변수를 선언하기도 전에 정의가 가능함. 
+//var hoisting (move declartion from bottom to top)
+//has no block scope 블록 영향을 안 받음
+console.log(age);
+age = 5;
+console.log(age);
+var age
+
+//3. Constants
+//favor immutable data type always for a few reasons:
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+console.log(daysInWeek);
+
+//4. Variable types 
+// primitive, single item: number, string, boolean, null, undefiedn, symbol
+// objict, box container
+// function, first-class function 
+
+const count = 17; //integer
+const size = 17.1; //decimal number
+console.log(`value: ${count}, type: ${typeof count}`);
+console.log(`value: ${size}, type: ${typeof size}`);
+
+//number - special numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a nuber' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+//bigInt (fairly new, don't use it yet) 추가된지 얼마 안 됨. 지원 브라우저 별로 없음.
+const bigInt = 1234567890123456789012345678901234567890n; //over (-2**53) ~ (2*53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+
+//string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!`; //template literals (string) 백틱 기호를 이용하여 문자열 띄어쓰기 편하게
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
